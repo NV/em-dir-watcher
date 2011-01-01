444f4a4aea4a8d70903a7a5f27e342dc1012da55
Nikita Vasilyev <me@elv1s.ru>
2011-01-01 19:18:45 +0300

    An half-assed attempt to replace rubycocoa with rb-fsevent. Most tests fail.

--- lib/em-dir-watcher/platform/mac.rb
+++ lib/em-dir-watcher/platform/mac.rb
@@ -12,16 +12,22 @@ class Watcher
     attr_accessor :handler, :active
 
     def initialize path, inclusions, exclusions
+        update_last_event
+
         subprocess = lambda do |ready, output|
-            require "em-dir-watcher/platform/mac/rubycocoa_watcher"
-            # require "em-dir-watcher/platform/mac/ffi_fsevents_watcher"
-            stream = FSEventStream.new [path] do |changed_paths|
-                changed_paths.each { |path| output.call path }
+            require 'rb-fsevent'
+            stream = FSEvent.new
+            stream.watch path do |directories|
+              files = find_changed_files(directories, {:all => true})
+              files.each { |path|
+                output.call path
+              }
             end
+
             ready.call()
-            stream.run_loop
+            stream.run
         end
-        
+
         @invoker = EMDirWatcher::Invokers::SubprocessInvoker.new subprocess do |path|
             code, path = path[0], path[1..-1]
             if code == ?> || code == ?-
@@ -43,6 +49,30 @@ class Watcher
     def stop
         @invoker.stop
     end
+
+    # Copy/paste from https://github.com/guard/guard/blob/a21bb8e306e44fd153c8b88a7c59c00e451b1d52/lib/guard/listener.rb#L34-54
+    private
+
+      def find_changed_files(dirs, options = {})
+        files = potentially_changed_files(dirs, options).select { |path| File.file?(path) && changed_file?(path) }
+        files.map! { |file| file.gsub("#{Dir.pwd}/", '') }
+      end
+
+      def potentially_changed_files(dirs, options = {})
+        match = options[:all] ? "**/*" : "*"
+        result = Dir.glob(dirs.map { |dir| "#{dir}#{match}" })
+        result
+      end
+
+      def changed_file?(file)
+        File.mtime(file) >= @last_event
+      rescue
+        false
+      end
+
+      def update_last_event
+        @last_event = Time.now
+      end
 
 end
 end
